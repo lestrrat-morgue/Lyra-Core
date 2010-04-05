042552db8d42f31a4fe554da733ec74030561baa
lestrrat <lestrrat+github@gmail.com>
2010-04-05 18:57:24 +0900

    refactor

--- lib/Lyra/Trait/App/WithLogger.pm
+++ lib/Lyra/Trait/App/WithLogger.pm
@@ -15,66 +15,68 @@ role {
     foreach my $logger (@{ $p->loggers }) {
         my $prefix = $logger->{prefix};
 
-        has "${prefix}_log_class" => (
-            is => 'ro',
-            isa => 'Str',
-            default => 'File',
-            documentation => "Class for logging $prefix. Defaut is File",
+        my @opts = (
+            {
+                name => "log_class",
+                default => "File",
+                documentation => "Class for logging $prefix. Defaut is File",
+            },
+            {
+                name => "log_disable",
+                isa => 'Bool',
+                default => 0,
+                documentation => "Flag to enable/disable $prefix logging",
+            },
+            {
+                name => "log_prefix",
+                default => "$prefix.",
+                documentation => "Prefix to use for log file names for $prefix"
+            },
+            {
+                name => "log_dsn",
+                documentation => "DSN for logging $prefix via Q4M",
+            },
+            {
+                name => "log_user",
+                documentation => "Username for logging $prefix via Q4M",
+            },
+            {
+                name => "log_password",
+                documentation => "Password for logging $prefix via Q4M",
+            },
+            {
+                name => "log_table",
+                default => "${prefix}_log_queue",
+                documentation => "Table name for logging $prefix via Q4M",
+            },
+            {
+                name => "log_sql",
+                documentation => "SQL to use for logging $prefix via Q4M",
+            }
         );
+   
+        foreach my $opt (@opts) {
+            my $name = "${prefix}_$opt->{name}";
+            my $flag = $name;
+            $flag =~ s/_/-/g;
+            has $name => (
+                traits => [ 'Getopt' ],
+                is => $opt->{is} || 'ro',
+                isa => $opt->{isa} || 'Str',
+                default => exists $opt->{default} ? $opt->{default} : 'File',
+                cmd_flag => $flag,
+                documentation => $opt->{documentation},
+            );
+        }
     
-        has "${prefix}_log_disable" => (
-            is => 'ro',
-            isa => 'Bool',
-            default => 0,
-            documentation => "Flag to enable/disable $prefix logging",
-        );
-    
-        has "${prefix}_log_prefix" => (
-            is => 'ro',
-            isa => 'Str',
-            default => "$prefix.",
-            documentation => "Prefix to use for log file names for $prefix"
-        );
-    
-        has "${prefix}_log_dsn" => (
-            is => 'ro',
-            isa => 'Str',
-            documentation => "DSN for logging $prefix via Q4M",
-        );
-    
-        has "${prefix}_log_user" => (
-            is => 'ro',
-            isa => 'Str',
-            documentation => "Username for logging $prefix via Q4M",
-        );
-
-        has "${prefix}_log_password" => (
-            is => 'ro',
-            isa => 'Str',
-            documentation => "Password for logging $prefix via Q4M",
-        );
-
-        has "${prefix}_log_table" => (
-            is => 'ro',
-            isa => 'Str',
-            default => "${prefix}_log_queue",
-            documentation => "Table name for logging $prefix via Q4M",
-        );
-
-        has "${prefix}_log_dsn" => (
-            is => 'ro',
-            isa => 'Str',
-            documentation => "SQL to use for logging $prefix via Q4M",
-        );
-    
-        my $p_log_class = "${prefix}_log_class";
-        my $p_log_disable = "${prefix}_log_disable";
-        my $p_log_prefix = "${prefix}_log_prefix";
-        my $p_log_dsn = "${prefix}_log_dsn";
-        my $p_log_user = "${prefix}_log_user";
+        my $p_log_class    = "${prefix}_log_class";
+        my $p_log_disable  = "${prefix}_log_disable";
+        my $p_log_prefix   = "${prefix}_log_prefix";
+        my $p_log_dsn      = "${prefix}_log_dsn";
+        my $p_log_user     = "${prefix}_log_user";
         my $p_log_password = "${prefix}_log_password";
-        my $p_log_table = "${prefix}_log_table";
-        my $p_log_sql = "${prefix}_log_sql";
+        my $p_log_table    = "${prefix}_log_table";
+        my $p_log_sql      = "${prefix}_log_sql";
 
         method "build_${prefix}_log" => sub {
             my $self = shift;
