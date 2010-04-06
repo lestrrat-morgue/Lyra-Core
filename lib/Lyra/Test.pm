8f14d458a9c8804d77f93b6f029853a27181fcaa
lestrrat <lestrrat+github@gmail.com>
2010-04-06 14:16:08 +0900

    Add test files

---
+++ lib/Lyra/Test.pm
@@ -0,0 +1,108 @@
+package Lyra::Test;
+use strict;
+use base qw(Exporter);
+use AnyEvent::DBI;
+use Cache::Memcached::AnyEvent;
+use Lyra::Test::Plackup;
+use Lyra::Test::Fixture::Daemons;
+
+our @EXPORT_OK = qw(start_daemons async_dbh click_server adengine_byarea find_program);
+
+sub null_log {
+    require Lyra::Log::Storage::Null;
+    Lyra::Log::Storage::Null->new();
+}
+
+sub start_daemons(@) {
+    my $guard = Lyra::Test::Fixture::Daemons->new();
+    $guard->start();
+    return $guard;
+}
+
+sub async_dbh(@) {
+    return AnyEvent::DBI->new(
+        $ENV{TEST_DSN},
+        $ENV{TEST_USERNAME},
+        $ENV{TEST_PASSWORD},
+        exec_server => 1,
+        RaiseError => 1,
+        AutoCommit => 1,
+    );
+}
+
+sub async_memcached(@) {
+    return Cache::Memcached::AnyEvent->new(
+        servers => [ "127.0.0.1:$ENV{ TEST_MEMCACHED_PORT }" ],
+        compress_threshld => 10_000,
+        namespace => join('.', 'lyra', 'test', $$, {}, rand())
+    );
+}
+
+sub plackup(@) {
+    my $plackup = Lyra::Test::Plackup->new(@_);
+    $plackup->start or die "Could not start server";
+    return $plackup;
+}
+
+sub click_server(@) {
+    return plackup(
+        base_dir => 't/',
+        server => 'Twiggy',
+        app => sub {
+            require Lyra::Server::Click;
+
+            Lyra::Server::Click->new(
+                dbh => async_dbh(),
+                log_storage => null_log(),
+            )->psgi_app
+        },
+        @_,
+    );
+}
+
+sub adengine_byarea(@) {
+    my %args = @_;
+    my $click_uri = delete $args{click_server}
+        or die "You need to specify a click server URL";
+    my $templates_dir = delete $args{templates_dir} || 'templates';
+    my $request_log = delete $args{request_log};
+    my $impression_log = delete $args{impression_log};
+    return plackup(
+        base_dir => 't/',
+        server => 'Twiggy',
+        app => sub {
+            require Lyra::Server::AdEngine::ByArea;
+
+            Lyra::Server::AdEngine::ByArea->new(
+                dbh => async_dbh(),
+                cache => async_memcached(),
+                click_uri => $click_uri,
+                request_log_storage => $request_log || null_log(),
+                impression_log_storage => $impression_log || null_log(),
+                templates_dir => $templates_dir,
+            )->psgi_app
+        },
+        %args,
+    );
+}
+
+sub find_program($) {
+    my $prog = shift;
+    my $path = _get_path_of($prog);
+    return $path
+        if $path;
+    die "could not find $prog, please set appropriate PATH";
+}
+
+sub _get_path_of {
+    my $prog = shift;
+    my $path = `which $prog 2> /dev/null`;
+    chomp $path
+        if $path;
+    $path = ''
+        unless -x $path;
+    $path;
+}
+
+
+1;
---
+++ lib/Lyra/Test/Fixture/TestDB.pm
@@ -0,0 +1,63 @@
+package Lyra::Test::Fixture::TestDB;
+use Moose;
+use namespace::autoclean;
+
+has adserver_uri => (
+    is => 'ro',
+    isa => 'Str',
+    default => 'http://127.0.0.1/'
+);
+
+sub deploy {
+    my ($self, $schema) = @_;
+
+    my $master_rs = $schema->resultset('AdsMaster');
+
+    # XXX use some other method if you want to bulk insert 
+    my $count = 1;
+    my @ads = map {
+        $_->{id} ||= sprintf('test_ads_by_area%03d', $count++);
+        $_->{landing_uri} ||= "http://127.0.0.1/$_->{id}";
+        $_->{status} = 1 unless exists $_->{status};
+        $_;
+    } (
+        {
+            title => 'オペラシティ',
+            content => '最寄り駅は初台です',
+            location => \q|GeomFromText('POINT(139.685945 35.683616)')|,
+        },
+        {
+            title => 'NTT東日本',
+            content => '最寄り駅は初台です',
+            location => \q|GeomFromText('POINT(139.678481 35.689265)')|,
+        },
+        {
+            title => '幡ヶ谷駅',
+            content => '初台のとなりです',
+            location => \q|GeomFromText('POINT(139.674506 35.678603)')|,
+        },
+        {
+            title => '明治大学',
+            content => '最寄り駅は明大前です',
+            location => \q|GeomFromText('POINT(139.641874 35.675566)')|,
+        },
+        {
+            title => '渋谷駅',
+            content => '南口の使いにくさは異常です',
+            location => \q|GeomFromText('POINT(139.703946 35.657775)')|,
+        },
+        {
+            title => '三軒茶屋駅',
+            content => 'こちらのハナマサの魚は結構質がいいですね',
+            location => \q|GeomFromText('POINT(139.700174 35.656826)')|,
+        },
+    );
+
+    foreach my $ad (@ads) {
+        $master_rs->create( $ad );
+    }
+}
+
+__PACKAGE__->meta->make_immutable();
+
+1;
